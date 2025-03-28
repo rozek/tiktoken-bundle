220af0cdc3e345d9ca35d061b1f38346226b8739
Andreas Rozek <A.Rozek@gmx.de>
2025-03-28 22:32:03 +0100

    initial build

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+/**** TokensOfText ****/
+export declare function TokensOfText(Text: string): number[];
+/**** TextFromTokens ****/
+export declare function TextFromTokens(TokenList: number[]): string;
+/**** NumberOfTokensInText ****/
+export declare function NumberOfTokensInText(Text: string): number;
+export declare function TokenizationOfText(Text: string): [number, string][];
